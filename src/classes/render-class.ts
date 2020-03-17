3fb84db3ab511d566b34fceb242177da170c661e
Nolan <noltron000@gmail.com>
2020-03-17 09:44:14 -0700

    [upd] using class model in state

---
+++ src/classes/render-class.ts
@@ -0,0 +1,12 @@
+class RenderClass {
+	text: string
+	constructor () {
+		this.text = 'hello, world'
+	}
+
+	addPoint () {
+		this.text += '!'
+	}
+}
+
+export default RenderClass
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 import './App.css';
+import RenderClass from '../classes/render-class'
+
+const myRender = new RenderClass()
 
 const App = () => {
-	const [words, setWords] = useState('Hello, world')
+	const [words, setWords] = useState(myRender.text)
 	return (
 		<>
 			<h1>Maze Generator</h1>
 			<p>
 				Generate your maze by clicking the button.
-				<br />
-				<input
-					type='button'
-					onClick={() => setWords(words + '!')}
-					value='Generate'
-				/>
 			</p>
+			<input
+				type='button'
+				onClick={() => {
+					myRender.addPoint()
+					setWords(myRender.text)
+				}}
+				value='Generate'
+			/>
 			<textarea
 				className='results'
 				value={words}
