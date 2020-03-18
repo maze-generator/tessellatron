7ad295a6b54b7920089954b2c96e05dcf7baff43
Nolan <noltron000@gmail.com>
2020-03-18 11:47:02 -0700

    [upd] refactored `Cell` in `cell.ts`

--- src/classes/cell.ts
+++ src/classes/cell.ts
@@ -1,23 +1,49 @@
 class Cell {
-	neighbors: {[key:string]:Cell|undefined|null}
-	constructor () {
-		this.neighbors = {
-			'north': undefined,
-			'south': undefined,
-			'east': undefined,
-			'west': undefined,
+	position:number
+	compass:{[key:string]:string}
+	pathways:{[key:string]:boolean}
+	neighbors:{[key:string]:Cell|undefined|null}
+	constructor (
+		compass:{[key:string]:string},
+		position:number,
+	) {
+		// initialize basic information.
+		this.position = position
+		this.compass = compass
+
+		// initialize pathways & neighbors.
+		this.pathways = {}
+		this.neighbors = {}
+		for (const direction in this.compass) {
+			this.pathways[direction] = false
+			this.neighbors[direction] = undefined
 		}
+	}
+
+	hasAnyPath ():boolean {
+		// a direction is either a wall (false) or path (true).
+		// check if there's any paths in the values.
+		return Object.values(this.pathways).includes(true)
+		// `.values()` makes a list of booleans from pathways.
+		// `.includes()` creates a boolean, which is returned.
 	}
 
 	hasNeighbor (that:Cell):boolean {
 		// check if this is a neighbor of that.
-		for (const neighbor of Object.values(this.neighbors)) {
-			if (neighbor === that) {
-				return true
-			}
-		}
-		// loop completed; that neighbor not found.
-		return false
+		return Object.values(this.neighbors).includes(that)
+		// `.values()` makes a list of cells from neighbors.
+		// `.includes()` creates a boolean, which is returned.
+	}
+
+	joinWithNeighbor (
+		that:Cell,
+		direction:string,
+	):void {
+		// mirroring is the opposite of a direction.
+		// for example, the mirror of north is south.
+		const mirroring:string = this.compass[direction]
+		this.neighbors[direction] = that
+		that.neighbors[mirroring] = this
 	}
 }
 
