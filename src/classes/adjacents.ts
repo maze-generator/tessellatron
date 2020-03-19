164d535ff2c13574a61542b8981e119fc0d7550c
Nolan <noltron000@gmail.com>
2020-03-19 15:26:46 -0700

    [upd] `adjacents.ts` got more complicated

--- src/classes/adjacents.ts
+++ src/classes/adjacents.ts
@@ -1,34 +1,54 @@
-import Compass from './compass'
+// a `Tensor` is a nth-dimensional array of numbers.
+// '0' is a valid `Tensor`, as is `[[2,3],[2,5]]`.
+type Tensor = Array<Tensor>|number
+
+
+// a `Slicer` callback finds an nth-dimensional coordinate.
+// it returns a cell, a row of cells, or a plane of cells.
+type Slicer = (
+	positions:Array<number|undefined>,
+	dimensions:Array<number>,
+) => (Tensor)
 
 // a `Locator` callback finds neighboring addresses.
 // such an address takes on the form of an index.
 // one can use such an index to find a cell in `Map`.
 type Locator = (
 	index:number,
-	...dimensions:Array<number>
+	dimensions:Array<number>
 ) => (number)
 
-class Adjacents extends Compass {
-	private readonly triangulators:{[key:string]:Locator}
+class Tensormogrifier {
+	public shape:string
 	private readonly dimensions:Array<number>
+	private readonly slicer:Slicer
+	private readonly triangulators:{[key:string]:Locator}
 
 	constructor (
-		opposites:{[key:string]:string},
+		shape:string,
+		dimensions:Array<number>,
+		slicer:Slicer,
 		triangulators:{[key:string]:Locator},
-		...dimensions:Array<number>
 	) {
-		super(opposites)
-		this.triangulators = triangulators
+		this.shape = shape
 		this.dimensions = dimensions
+		this.slicer = slicer
+		this.triangulators = triangulators
 	}
 
-	public lookup (
+	public getNeighborIndex (
 		index:number,
 		direction:string,
 	):number {
 		const callback:Locator = this.triangulators[direction]
-		return callback(index, ...this.dimensions)
+		return callback(index, this.dimensions)
+	}
+
+	public getSlicedTensor (
+		...positions:Array<number|undefined>,
+	):Tensor {
+		return this.slicer(positions, this.dimensions)
 	}
 }
 
-export default Adjacents
+export default Tensormogrifier
