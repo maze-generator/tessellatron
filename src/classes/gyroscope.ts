db53b34720d1ae7dc88e28848a4ba9019b221db6
Nolan Kovacik <noltron000@gmail.com>
2020-03-23 16:19:35 -0400

    [upd] `gyroscope.ts`

--- src/classes/gyroscope.ts
+++ src/classes/gyroscope.ts
@@ -1,46 +1,49 @@
+import Compass, {Rose} from './compass'
+
 // a `Slicer` callback finds an nth-dimensional coordinate.
 // it returns a cell, a row of cells, or a plane of cells.
 export type Slicer = (
-	positions:Array<number|undefined>,
 	dimensions:Array<number>,
+	positions:Array<number|undefined>,
 ) => Array<number>
 
 // a `Locator` callback finds neighboring addresses.
 // such an address takes on the form of an index.
 // one can use such an index to find a cell in `Map`.
 export type Locator = (
+	dimensions:Array<number>,
 	index:number,
-	dimensions:Array<number>
 ) => number
 
-class Gyroscope {
+class Gyroscope extends Compass {
 	private readonly dimensions:Array<number>
 	private readonly slicer:Slicer
-	private readonly locators:{[key:string]:Locator}
-
+	private readonly locator:Locator
 	constructor (
+		rose:Rose,
 		dimensions:Array<number>,
 		slicer:Slicer,
-		locators:{[key:string]:Locator},
+		locator:Locator,
 	) {
+		super(rose)
 		this.dimensions = dimensions
 		this.slicer = slicer
-		this.locators = locators
+		this.locator = locator
 	}
 
-	public getNeighborIndex (
-		index:number,
-		direction:string,
-	):number {
-		const callback:Locator = this.locators[direction]
-		return callback(index, this.dimensions)
-	}
+	// public getNeighborIndex (
+	// 	index:number,
+	// 	direction:string,
+	// ):number {
+	// 	const callback:Locator = this.locators[direction]
+	// 	return callback(index, this.dimensions)
+	// }
 
-	public getSlicedTensor (
-		...positions:Array<number|undefined>
-	):Tensor {
-		return this.slicer(positions, this.dimensions)
-	}
+	// public getSlicedTensor (
+	// 	...positions:Array<number|undefined>
+	// ):Tensor {
+	// 	return this.slicer(positions, this.dimensions)
+	// }
 }
 
 export default Gyroscope
