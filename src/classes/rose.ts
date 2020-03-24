291df73bdc489794dd953b6f0cd3adbd7ddeeb6d
Nolan Kovacik <noltron000@gmail.com>
2020-03-23 23:06:25 -0400

    [upd] feature-complete rose

--- src/classes/rose.ts
+++ src/classes/rose.ts
@@ -1,17 +1,63 @@
-class Rose {
+export default class Rose {
 	protected readonly magnitudes:Array<number>
-	public readonly rose:{[key:string]:number}
+	protected readonly rose:{[key:string]:number}
 	constructor (
 		protected readonly dimensions:Array<number>
 	) {
 		// create a multiplier affect for magnitudes.
 		const magnitudes = []
+		const multiplier = (a:number, b:number):void => a * b
 		for (const index in Object.keys(dimensions)) {
 			magnitudes.push(
-				dimensions.slice(0, index).reduce((a, b) => a * b, 1)
+				dimensions.slice(0, index).reduce(multiplier, 1)
 			)
 		}
 		this.magnitudes = magnitudes
+	}
+
+	private set rose (
+		rose:{[key:string]:number}
+	):{[key:string]:number} {
+		// `directions` is a simple set of named vectors.
+		// luckily, these are exactly the keys of `rose`.
+		this.directions = new Set(Object.keys(rose))
+
+
+		// `opposites` is a harder nut to crack.
+		// the app reverses the keys into a JavaScript Map.
+		// since the keys are numbers, they can be inverted.
+		// use that to find the opposing direction string.
+		const reducer = (
+			map:Map<number, string>,
+			direction:string,
+		):Map<number, string> => {
+			// compute the `vector` of the direction.
+			map.set(rose[direction], direction)
+			return map
+		}
+
+		const vectors:Map<number, string> = (
+			// set the `vectors` into a map with reduce.
+			Object.keys(rose).reduce(reducer, new Map())
+		)
+
+		// initialize opposites.
+		this.opposites = {}
+		for (const direction in this.directions) {
+			const vector:number = this.rose[direction]
+			const reversed:string = vectors.get(-vector)
+			// here is where reverse-directions is set!
+			this.opposites[direction] = reversed
+		}
+
+		// finally, just return the rose to set.
+		return rose
+	}
+
+	public reverse(direction:string):string {
+		// directly pulling from `opposites` would be easy...
+		// ...but, it seems more semantic using a method here!
+		return this.opposites[direction]
 	}
 }
 
@@ -19,6 +65,7 @@ class Rose {
 // a quadrilateral is a four-angled polygon.
 // they mean the same thing.
 export class TetragonRose extends Rose {
+	private readonly magnitudes:Array<number>
 	constructor (
 		dimensions:Array<number>
 	) {
@@ -37,6 +84,7 @@ export class TetragonRose extends Rose {
 
 // a hexahedron is a six-sided polyhedron.
 export class HexahedronRose extends Rose {
+	private readonly magnitudes:Array<number>
 	constructor (
 		dimensions:Array<number>
 	) {
@@ -57,6 +105,7 @@ export class HexahedronRose extends Rose {
 
 // a hexahedron is a six-sided polygon.
 export class HexagonRose extends Rose {
+	private readonly magnitudes:Array<number>
 	constructor (
 		dimensions:Array<number>
 	) {
@@ -74,3 +123,12 @@ export class HexagonRose extends Rose {
 		}
 	}
 }
+
+/***************************NOTES***************************
+==NOTE==
+- `Rose` is a default class so that it can be typecast.
+
+==TODO==
+- the for loop in the primary class is suboptimal.
+- `this.magnitudes` is not the greatest name; rename it.
+***********************************************************/
