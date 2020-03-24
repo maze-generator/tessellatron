f037b0489f3ddb8c051a100cad78dbdeb314e1d1
Nolan Kovacik <noltron000@gmail.com>
2020-03-23 20:52:19 -0400

    [upd] rose is getting complex

--- src/classes/compass.ts
+++ src/classes/compass.ts
@@ -78,29 +78,71 @@ export default class Compass {
 // a tetragon is a four-sided polygon.
 // a quadrilateral is a four-angled polygon.
 // they mean the same thing.
-export const tetragons:Rose = {
-	'north': (x:number, y:number):number => -y,
-	'south': (x:number, y:number):number => +y,
-	'east':  (x:number, y:number):number => +x,
-	'west':  (x:number, y:number):number => -x,
+export const tetragons = (
+	dimensions:Array<number>
+):{string:number} => {
+	// create a multiplier affect for mySlice.
+	const mySlice = []
+	for (const index in Object.keys(dimensions)) {
+		mySlice.push(
+			dimensions.slice(0, index).reduce((a, b) => a * b, 1)
+		)
+	}
+	// deconstruct dimensions into proper variables.
+	const [x, y] = mySlice
+	// return a rose of index-offsetters.
+	return {
+		'north': -y,
+		'south': +y,
+		'east':  +x,
+		'west':  -x,
+	}
 }
 
 // a hexahedron is a six-sided polyhedron.
-export const hexahedrons:Rose = {
-	'above': (x:number, y:number, z:number) => -z,
-	'below': (x:number, y:number, z:number) => +z,
-	'north': (x:number, y:number, z:number) => -y,
-	'south': (x:number, y:number, z:number) => +y,
-	'east':  (x:number, y:number, z:number) => -x,
-	'west':  (x:number, y:number, z:number) => +x,
+export const hexahedrons = (
+	dimensions:Array<number>
+):{string:number} => {
+	// create a multiplier affect for mySlice.
+	const mySlice = []
+	for (const index in Object.keys(dimensions)) {
+		mySlice.push(
+			dimensions.slice(0, index).reduce((a, b) => a * b, 1)
+		)
+	}
+	// deconstruct dimensions into proper variables.
+	const [x, y, z] = mySlice
+	// return a rose of index-offsetters.
+	return {
+		'above': -z,
+		'below': +z,
+		'north': -y,
+		'south': +y,
+		'east':  -x,
+		'west':  +x,
+	}
 }
 
 // a hexahedron is a six-sided polygon.
-export const hexagons:Rose = {
-	'northwest': (x:number, y:number):number => -y,
-	'southeast': (x:number, y:number):number => +y,
-	'northeast': (x:number, y:number):number => x - y,
-	'southwest': (x:number, y:number):number => y - x,
-	'east': (x:number, y:number):number => -x,
-	'west': (x:number, y:number):number => +x,
+export const hexagons = (
+	dimensions:Array<number>
+):{string:number} => {
+	// create a multiplier affect for mySlice.
+	const mySlice = []
+	for (const index in Object.keys(dimensions)) {
+		mySlice.push(
+			dimensions.slice(0, index).reduce((a, b) => a * b, 1)
+		)
+	}
+	// deconstruct dimensions into proper variables.
+	const [x, y] = mySlice
+	// return a rose of index-offsetters.
+	return {
+		'northwest': -y,
+		'southeast': +y,
+		'northeast': x - y,
+		'southwest': y - x,
+		'east': -x,
+		'west': +x,
+	}
 }
