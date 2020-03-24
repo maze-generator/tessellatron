7bdcef30399fb24f0f3f1dfbe7fa781147b63ca6
Nolan Kovacik <noltron000@gmail.com>
2020-03-24 11:49:03 -0400

    [upd] visualize maze

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-import RenderClass from '../classes/render-class'
 import Maze from '../classes/maze'
+import Graphic from '../classes/unicode-graphic'
 
-const myMaze = new Maze([4,5])
-const myRender = new RenderClass()
+const myMaze = new Maze([40,50])
+const myGraphic = new Graphic(myMaze)
 
 console.log(myMaze)
 
 const App = () => {
-	const [words, setWords] = useState(myRender.text)
+	const [words, setWords] = useState(myGraphic.pipeMaze())
 	return (
 		<>
 			<h1>Maze Generator</h1>
@@ -19,11 +19,13 @@ const App = () => {
 			<input
 				type='button'
 				onClick={() => {
-					myRender.addPoint()
-					setWords(myRender.text)
+					const newMaze = new Maze([100,20])
+					const newGraphic = new Graphic(newMaze)
+					setWords(newGraphic.pipeMaze())
 				}}
 				value='Generate'
 			/>
+
 			<textarea
 				className='results'
 				value={words}
