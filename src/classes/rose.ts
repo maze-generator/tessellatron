0805ae82ca365eccda627b0c6a8f2247ce16fb4b
Nolan Kovacik <noltron000@gmail.com>
2020-03-23 21:10:26 -0400

    [add] awesome Compass `Rose` class

--- src/classes/rose.ts
+++ src/classes/rose.ts
@@ -1,73 +1,76 @@
-export type Rose = {string:number[]}
+class Rose {
+	protected readonly magnitudes:Array<number>
+	public readonly rose:{[key:string]:number}
+	constructor (
+		protected readonly dimensions:Array<number>
+	) {
+		// create a multiplier affect for magnitudes.
+		const magnitudes = []
+		for (const index in Object.keys(dimensions)) {
+			magnitudes.push(
+				dimensions.slice(0, index).reduce((a, b) => a * b, 1)
+			)
+		}
+		this.magnitudes = magnitudes
+	}
+}
 
 // a tetragon is a four-sided polygon.
 // a quadrilateral is a four-angled polygon.
 // they mean the same thing.
-export const tetragons = (
-	dimensions:Array<number>
-):{string:number} => {
-	// create a multiplier affect for mySlice.
-	const mySlice = []
-	for (const index in Object.keys(dimensions)) {
-		mySlice.push(
-			dimensions.slice(0, index).reduce((a, b) => a * b, 1)
-		)
-	}
-	// deconstruct dimensions into proper variables.
-	const [x, y] = mySlice
-	// return a rose of index-offsetters.
-	return {
-		'north': -y,
-		'south': +y,
-		'east':  +x,
-		'west':  -x,
+export class TetragonRose extends Rose {
+	constructor (
+		dimensions:Array<number>
+	) {
+		super(dimensions)
+		// deconstruct magnitudes for each axis.
+		const [x, y] = this.magnitudes
+		// generate a rose of index-offsetters.
+		this.rose = {
+			'north': -y,
+			'south': +y,
+			'east':  +x,
+			'west':  -x,
+		}
 	}
 }
 
 // a hexahedron is a six-sided polyhedron.
-export const hexahedrons = (
-	dimensions:Array<number>
-):{string:number} => {
-	// create a multiplier affect for mySlice.
-	const mySlice = []
-	for (const index in Object.keys(dimensions)) {
-		mySlice.push(
-			dimensions.slice(0, index).reduce((a, b) => a * b, 1)
-		)
-	}
-	// deconstruct dimensions into proper variables.
-	const [x, y, z] = mySlice
-	// return a rose of index-offsetters.
-	return {
-		'above': -z,
-		'below': +z,
-		'north': -y,
-		'south': +y,
-		'east':  -x,
-		'west':  +x,
+export class HexahedronRose extends Rose {
+	constructor (
+		dimensions:Array<number>
+	) {
+		super(dimensions)
+		// deconstruct magnitudes for each axis.
+		const [x, y, z] = this.magnitudes
+		// generate a rose of index-offsetters.
+		this.rose = {
+			'above': -z,
+			'below': +z,
+			'north': -y,
+			'south': +y,
+			'east':  -x,
+			'west':  +x,
+		}
 	}
 }
 
 // a hexahedron is a six-sided polygon.
-export const hexagons = (
-	dimensions:Array<number>
-):{string:number} => {
-	// create a multiplier affect for mySlice.
-	const mySlice = []
-	for (const index in Object.keys(dimensions)) {
-		mySlice.push(
-			dimensions.slice(0, index).reduce((a, b) => a * b, 1)
-		)
-	}
-	// deconstruct dimensions into proper variables.
-	const [x, y] = mySlice
-	// return a rose of index-offsetters.
-	return {
-		'northwest': -y,
-		'southeast': +y,
-		'northeast': x - y,
-		'southwest': y - x,
-		'east': -x,
-		'west': +x,
+export class HexagonRose extends Rose {
+	constructor (
+		dimensions:Array<number>
+	) {
+		super(dimensions)
+		// deconstruct magnitudes for each axis.
+		const [x, y] = this.magnitudes
+		// generate a rose of index-offsetters.
+		this.rose = {
+			'northwest': -y,
+			'southeast': +y,
+			'northeast': x - y,
+			'southwest': y - x,
+			'east': -x,
+			'west': +x,
+		}
 	}
 }
