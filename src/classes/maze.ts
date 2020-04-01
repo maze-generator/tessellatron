47d9d3f663e5bbaf1ca4dbdf83616763ef4f3aca
Nolan Kovacik <noltron000@gmail.com>
2020-04-01 01:21:38 -0400

    [upd] slight move in maze constructor

--- src/classes/maze.ts
+++ src/classes/maze.ts
@@ -19,21 +19,21 @@ export default class Maze {
 		// create a mini-multiplier reducer function.
 		const multiplier = (a:number, b:number):number => a * b
 
-		// calculate size of the maze, aka the number of cells.
-		this.size = dimensions.reduce(multiplier)
-
 		// the magnitudes are how much an index must move as
 		// to offset an associated coordinate by exactly one.
 		this.magnitudes = []
 		// loop through dimensions via each index, `i`.
 		for (let i:number = 0; i < dimensions.length; i += 1) {
 			// collect antecedent dimensions leading up to this.
-			const previous:number[] = dimensions.slice(0, i)
+			const previous:Array<number> = dimensions.slice(0, i)
 			// calculate the product of those dimensions.
 			const product:number = previous.reduce(multiplier, 1)
 			// add the product to the list of magnitudes.
 			this.magnitudes.push(product)
 		}
+
+		// calculate size of the maze, aka the number of cells.
+		this.size = dimensions.reduce(multiplier)
 
 		// the board begins blank.
 		this.map = new Array(this.size)
