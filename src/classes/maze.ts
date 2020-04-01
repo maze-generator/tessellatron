33d1d50294cf4fe323b48aa6c8d632e8366688a3
Nolan Kovacik <noltron000@gmail.com>
2020-04-01 01:19:04 -0400

    [upd] improve maze constructor

--- src/classes/maze.ts
+++ src/classes/maze.ts
@@ -1,21 +1,47 @@
 import Cell from './cell'
 import {TetragonCompass} from './compass'
-import {shuffle} from '../helpers/shuffle'
+import {shuffle} from '../helpers/random'
 
 export default class Maze {
-	dimensions:Array<number>
 	compass:TetragonCompass
+	dimensions:Array<number>
+	magnitudes:Array<number>
 	size:number
-	map:Array<Cell|undefined|null>
+	map:Array<Cell|null|undefined>
 	constructor(
 		dimensions:Array<number>,
 	) {
+
+		// dimensions represents the shape of the maze.
+		// eg length=3, width=4, height=5; or simply [3, 4, 5].
 		this.dimensions = dimensions
-		this.compass = new TetragonCompass(dimensions)
-		this.size = dimensions.reduce((a, b) => {
-			return a * b
-		})
+
+		// create a mini-multiplier reducer function.
+		const multiplier = (a:number, b:number):number => a * b
+
+		// calculate size of the maze, aka the number of cells.
+		this.size = dimensions.reduce(multiplier)
+
+		// the magnitudes are how much an index must move as
+		// to offset an associated coordinate by exactly one.
+		this.magnitudes = []
+		// loop through dimensions via each index, `i`.
+		for (let i:number = 0; i < dimensions.length; i += 1) {
+			// collect antecedent dimensions leading up to this.
+			const previous:number[] = dimensions.slice(0, i)
+			// calculate the product of those dimensions.
+			const product:number = previous.reduce(multiplier, 1)
+			// add the product to the list of magnitudes.
+			this.magnitudes.push(product)
+		}
+
+		// the board begins blank.
 		this.map = new Array(this.size)
+
+		// create compass
+		this.compass = new TetragonCompass(dimensions)
+
+		// TODO: generation shall not execute here.
 		this.generate(0)
 	}
 
@@ -40,8 +66,8 @@ export default class Maze {
 		}
 
 		// calculate coordinates.
-		const coordinates1:Array<number> = this.compass.triangulator(index1)
-		const coordinates2:Array<number> = this.compass.triangulator(index2)
+		const coordinates1:Array<number> = this.compass.triangulate(index1)
+		const coordinates2:Array<number> = this.compass.triangulate(index2)
 
 		// count the differences.
 		let differences:number = 0
