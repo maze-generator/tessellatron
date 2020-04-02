3f5ef7091901dc94bf8ddc88c1fbc6057acc1d0f
Nolan Kovacik <noltron000@gmail.com>
2020-04-02 14:31:13 -0400

    [upd] clean `maze.ts` constructor

--- src/classes/maze.ts
+++ src/classes/maze.ts
@@ -1,6 +1,11 @@
 import Cell from './cell'
 import {TetragonCompass} from './compass'
 import {shuffle} from '../helpers/random'
+import {
+	getSize,
+	getMagnitudes,
+} from '../helpers/project'
+
 
 export default class Maze {
 	compass:TetragonCompass
@@ -11,37 +16,20 @@ export default class Maze {
 	constructor(
 		dimensions:Array<number>,
 	) {
-
-		// dimensions represents the shape of the maze.
+		// `dimensions` represents the shape of the maze.
 		// eg length=3, width=4, height=5; or simply [3, 4, 5].
 		this.dimensions = dimensions
 
-		// create a mini-multiplier reducer function.
-		const multiplier = (a:number, b:number):number => a * b
+		// `magnitudes` are helpful to locate maze cells.
+		this.magnitudes = getMagnitudes(dimensions)
 
-		// the magnitudes are how much an index must move as
-		// to offset an associated coordinate by exactly one.
-		const magnitudes:Array<number> = []
-		// loop through dimensions via each index, `i`.
-		for (let i:number = 0; i < dimensions.length; i += 1) {
-			// collect antecedent dimensions leading up to this.
-			const previous:Array<number> = dimensions.slice(0, i)
-			// calculate the product of those dimensions.
-			const product:number = previous.reduce(multiplier, 1)
-			// add the product to the list of magnitudes.
-			magnitudes.push(product)
-		}
+		// the `size`, or number of cells, helps fill the map.
+		this.size = getSize(dimensions)
 
-		// apply calculated effects to this object.
-		this.magnitudes = magnitudes
-
-		// calculate size of the maze, aka the number of cells.
-		this.size = dimensions.reduce(multiplier)
-
-		// the maze board begins blank.
+		// the maze `map` begins blank.
 		this.map = new Array(this.size)
 
-		// create compass
+		// the `compass` orients each cell correctly.
 		this.compass = new TetragonCompass(dimensions)
 
 		// TODO: generation shall not execute here.
