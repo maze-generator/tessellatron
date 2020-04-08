60ff70a267a739cdc934ccd86695bf51793983fd
Nolan Kovacik <noltron000@gmail.com>
2020-04-08 14:21:42 -0400

    [add] `main.ts` base-class

--- src/classes/main.ts
+++ src/classes/main.ts
@@ -1,4 +1,19 @@
-// take inputs of size & layout
-// create a base map & compass template
-// create a base maze algorithm template
-// using maze algorithm, create cells, and insert to map.
+export default class Main {
+	constructor(
+		layout: string,
+		algorithm: string,
+		dimensions: Array<number>,
+	) {
+		// take inputs of size & layout.
+		this.layout = layout
+		this.dimensions = dimensions
+
+		// create a base map & compass template.
+		this.map = new Map()
+		this.compass = new Compass()
+
+		// create a base maze algorithm template.
+		// with maze algorithm, create cells, and insert to map.
+		this.generator = new Generator(algorithm)
+	}
+}
