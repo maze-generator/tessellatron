b250d25f122e8a469bf400cd251dc37c7b4bd9d1
Nolan Kovacik <noltron000@gmail.com>
2020-11-25 12:41:35 -0500

    [upd] default export only

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import Graph from './graph.js'
-import HypercubeGraph from './graph/hypercube.js'
-import Cell from './cell.js'
 
-// export those who should never have breaking changes.
-export {
-	Graph,
-	HypercubeGraph,
-	Cell,
-}
+// only export the primary functionality of the module.
+export default Graph
